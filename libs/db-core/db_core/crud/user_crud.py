dea200b2e24e5c993097e1350c714c30b5f635a6
Aditya Prakash <ap21prakash@gmail.com>
2025-07-10 15:22:15 +0000

    Delete operation was not awaited

--- libs/db-core/db_core/crud/user_crud.py
+++ libs/db-core/db_core/crud/user_crud.py
@@ -18,21 +18,21 @@ async def get_user(
         statement = statement.with_for_update()
     user = await session.exec(statement)
     if user:
-        return user
+        return user.first();
     else:
         return None
 
 
 async def update_user(
-    session: AsyncSession, db_user: User, user_in: UserUpdate
+    session: AsyncSession, user: User, user_in: UserUpdate
 ) -> User:
     update_data = user_in.model_dump(exclude_unset=True)
-    db_user.sqlmodel_update(update_data)
-    session.add(db_user)
-    return db_user
+    user.sqlmodel_update(update_data)
+    session.add(user)
+    return user
 
 
 async def delete_user(session: AsyncSession, user_id: str) -> None:
     user = await get_user(session, user_id)
     if user:
-        session.delete(user)
+        await session.delete(user)
